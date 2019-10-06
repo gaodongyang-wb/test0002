b605e2aa78a8480e9775d65f0760ad59474774f5
gaodongyang-wb <gaodongyang66@163.com>
2019-10-06 09:31:55 +0800

    222

---
+++ src/test001/model/User.java
@@ -0,0 +1,19 @@
+package test001.model;
+
+public class User {
+   private int age;
+   private int id;
+public int getAge() {
+	return age;
+}
+public void setAge(int age) {
+	this.age = age;
+}
+public int getId() {
+	return id;
+}
+public void setId(int id) {
+	this.id = id;
+}
+
+}
